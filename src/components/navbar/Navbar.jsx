06a3896c2c8d9295d6966ad5776963ca246ea511
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-06-07 21:56:36 +0530

    initial updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,43 @@
-import React, { Component } from 'react'
-import "./navbar.css"
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/av-logo.png';
+import './navbar.css';
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        Navbar
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} />
+        </div>
+        <div className="navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
-    )
-  }
-}
+      <div className="navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#000" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="navbar-menu_container scale-up-center">
+          <div className="navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
